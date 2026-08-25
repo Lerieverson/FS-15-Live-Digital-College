73963c7ba6915c08d76c67836de7b2398ce491ec
Leri_dio <lerieverson@gmail.com>
2026-08-25 20:14:27 -0300

    aula de estrutura de repetição

---
+++ javascript/estrutura-condicional/if else elseif.js
@@ -0,0 +1,17 @@
+let idade = 18;
+if(idade >= 19){
+  console.log("Você pode consumir bebida alcólicas")
+}
+else{
+  console.log("Você não pode consumir, pois ainda é menor de idade!!")
+}
+
+
+let semaforo = "Amarelo"
+const podePassar = "Verde"
+const atencao = "Amarelo"
+const pare = "Vermelho"
+
+if(semaforo == atencao){
+  console.log("Cuidado! O semaforo está amarelo")
+}
