d3d88ce65d39ffa2faec57fb0494440389f23ed9
Leri_dio <lerieverson@gmail.com>
2026-08-25 20:39:13 -0300

    aula de operações matemática

---
+++ javascript/operaradores-matematicos/operador-matematico.js
@@ -0,0 +1,24 @@
+// Soma de 2 numeros
+console.log("Soma de 4 + 2: ", 4 + 2)
+// Soma entre 3 números
+console.log('Soma de 4 + 2 + 4: ', 4 + 2 + 4);
+
+// Soma de 2 numeros
+console.log("Soma de 4 - 2: ", 4 - 2)
+// Soma entre 3 números
+console.log('Soma de 4 - 2 - 4: ', 4 - 2 - 4);
+
+// Soma de 2 numeros
+console.log("Soma de 4 * 2: ", 4 * 2)
+// Soma entre 3 números
+console.log('Soma de 4 * 2 * 4: ', 4 * 2 * 4);
+
+// Soma de 2 numeros
+console.log("Soma de 4 / 2: ", 4 / 2)
+// Soma entre 3 números
+console.log('Soma de 4 / 2 / 4: ', 4 / 2 / 4);
+
+// Soma de 2 numeros
+console.log("Soma de 4 % 2: ", 4 % 2)
+// Soma entre 3 números
+console.log('Soma de 4 % 3: ', 4 % 3);
